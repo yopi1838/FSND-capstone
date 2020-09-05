d17099f037209c4e53e1cd8ecb9bd29b03bc1735
yopi1838 <yopi1838@gmail.com>
2020-09-06 01:14:42 +0900

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgres://yopiprabowooktiovan@localhost:5432/castingagency'
+export DATABASE_URL='postgres://nklrsnuusnhyrt:7cf9611a35b2a3098de572708fafa5b84be9ca78c1365dc0373e8616462eb763@ec2-52-72-34-184.compute-1.amazonaws.com:5432/d96e1quen4qgsa'
 export AUTH0_DOMAIN='dev-eu13-nvj.us.auth0.com'
 export ALGORITHMS=['RS256']
 export API_AUDIENCE='castingagency'
